70305c566a57a29ffba68067eede744c50c5233f
Matthew F Leader <mleader@redhat.com>
2024-01-29 20:11:17 -0500

    gofmt try catch error

--- try/catch/error_by_value.go
+++ try/catch/error_by_value.go
@@ -9,7 +9,7 @@ import (
 // ErrorByValue creates an error handler that catches fixed value errors. For example, errors are typically
 // declared outside any functions like this:
 //
-//     var ErrMyError = fmt.Errorf("some value")
+//	var ErrMyError = fmt.Errorf("some value")
 func ErrorByValue(value error, f func(err error)) try.CatchHandler {
 	return func(e any) bool {
 		err, ok := e.(error)
